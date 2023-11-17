05c9695745d586b4f44454f96ca716523bf5319d
coopfeathy <cooperfeatherstone13@gmail.com>
2023-11-16 22:38:25 -0700

    display hello world on homepage

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
